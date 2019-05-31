722d61d31f5a2cac673b7a84114a2c9ad8d8e623
dasgprat <dasguptp@oregonstate.edu>
2019-05-31 14:29:14 -0700

    Adding minor product module changes

--- ProductExtension.hs
+++ ProductExtension.hs
@@ -18,32 +18,6 @@ instance Product' NewItem1 where
 
 -- | add new kind 
 
-class Product' p => NewKind' p where 
-    weight' :: p -> Int 
-
--- data NewKind = forall p. NewKind' p => NewKind p
-
--- pid         (NewKind p) = pid'  p
--- name        (NewKind p) = name' p
--- price       (NewKind p) = price'   p
--- description (NewKind p) = description' p
--- weight      (NewKind p) = weight' p
-
-
-data NewKind1 = NewKind1
-
--- newKind1 = NewKind NewKind1 
-instance Product' NewKind1 where 
-  pid'         _ = 5 
-  name'        _ = "NewKindName1"
-  price'       _ = 100
-  description' _ = "NewKindDescription1"    
-
-instance NewKind' NewKind1 where
-  weight'      _ = 10
-
-
-
 
 
 
