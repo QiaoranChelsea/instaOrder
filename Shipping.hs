87c81e455fd41ceb06f8fffb66e36b4ab7ab3851
dasgprat <dasguptp@oregonstate.edu>
2019-06-02 14:37:26 -0700

    Fixing shipping changes

---
+++ Shipping.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE ExistentialQuantification #-}
+module Shipping where 
+import Product
+
+
+
+class Shipping' m where
+  calculateShippingCost :: m -> Int -> Int
+
+-- | Shipping instance wrapper
+data Shipping = forall s. Shipping' s => Shipping s
+
+
+
+data Standard = Standard
+
+instance Shipping' Standard where
+  calculateShippingCost Standard w = 10 * w
+
+data Economy = Economy
+instance Shipping' Economy where
+  calculateShippingCost Economy w = 15 * w
+  
+
+data Prime = Prime
+instance Shipping' Prime where
+  calculateShippingCost Prime w = 18 * w
